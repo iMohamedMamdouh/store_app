beda73f4405562124b2843e147167184fabca410
iMohamedMamdouh <imohamedmamdoh1@gmail.com>
2024-07-25 17:12:40 +0200

    final services

--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -1,28 +1,18 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
 
 class GetAllProductService {
   Future<List<ProductModel>> getAllProduct() async {
-    http.Response response = await http.get(
-      Uri.parse('https://fakestoreapi.com/products'),
-    );
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products');
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      List<ProductModel> productsList = [];
+    List<ProductModel> productsList = [];
 
-      for (int i = 0; i < data.length; i++) {
-        productsList.add(
-          ProductModel.formJson(data[i]),
-        );
-      }
-
-      return productsList;
-    } else {
-      throw Exception(
-          'Failed to load products is State: ${response.statusCode}');
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(
+        ProductModel.fromJson(data[i]), // Corrected the method name here
+      );
     }
+    return productsList;
   }
 }
